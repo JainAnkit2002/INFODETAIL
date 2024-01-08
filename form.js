aa0df8d5333f4871b6785d28b8d7af278c20e3fb
JainAnkit2002 <152958478+JainAnkit2002@users.noreply.github.com>
2024-01-07 23:45:55 -0500

    hlo form detail

---
+++ form.js
@@ -0,0 +1,9 @@
+function onSubmit(){
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+
+    console.log("your name is=====>",name)
+    console.log("your age is=====>",age)
+   
+    console.log("form is submitted..")
+}
